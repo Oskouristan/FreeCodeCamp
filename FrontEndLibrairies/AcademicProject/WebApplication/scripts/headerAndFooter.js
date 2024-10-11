d9be34f413f5ddb282e062bde3fa8470ccd319c9
DariusBank <mdohdv@gmail.com>
2024-10-11 13:26:14 +0300

    Update on all pages

---
+++ FrontEndLibrairies/AcademicProject/WebApplication/scripts/headerAndFooter.js
@@ -0,0 +1,23 @@
+fetch('footer.html')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erreur lors du chargement du footer');
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    })
+    .catch(error => console.error('Erreur lors du chargement du footer:', error));
+
+fetch('header.html')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erreur lors du chargement du header');
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    })
+    .catch(error => console.error('Erreur lors du chargement du header:', error));
