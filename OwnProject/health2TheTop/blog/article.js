361d6d9ab692cb5edd97ccd24eaa4ada34dfa4d5
DariusBank <mdohdv@gmail.com>
2024-11-04 19:29:08 +0200

    "Articles.js Working Design not quite yet "

--- OwnProject/health2TheTop/blog/article.js
+++ OwnProject/health2TheTop/blog/article.js
@@ -1,44 +1,45 @@
-const chemin = "./articles.json";
+const path = "./articles.json";
+const homepath = "/start/OwnProject/health2TheTop/public/index.html";
 
-if (typeof marked !== 'undefined') {
-    alert('Marked est chargé avec succès !');
-} else {
-    alert('Marked n\'est pas défini !');
-}
 
 function getArticleIdFromURL() {
     const params = new URLSearchParams(window.location.search);
     return params.get('id');
 }
 
-function afficherArticle() {
-    const articleId = getArticleIdFromURL();
-    
-    fetch(chemin)
+function readArticlesJson() {
+    return fetch(path)
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Erreur HTTP : ${response.status}`);
             }
             return response.json();
-        })
-        .then(data => {
-            const article = data.find(item => item.id == articleId);
-
-            if (article) {
-                document.getElementById('article-container').innerHTML = `
-                    <div class="article-page" style="background-image: url(${article.image_url}); background-size: cover; color: white; padding: 20px;">
-                        <h1>${article.titre}</h1>
-                        <div class="article-content">${marked(article.texte)}</div>
-                    </div>
-                `;
-            } else {
-                document.getElementById('article-container').innerHTML = `<p>Article non trouvé.</p>`;
-            }
-        })
-        .catch(error => {
-            alert('Erreur lors de la récupération de l\'article : ' + error.message);
         });
 }
 
-// Appel de la fonction pour afficher l'article au chargement de la page
-afficherArticle();
+function showArticle(data) {
+    const articleId = getArticleIdFromURL();
+    const article = data.find(item => item.id == articleId);
+
+    if (article) {
+        document.getElementById('article-container').innerHTML = `
+            <h1>${article.titre}</h1>
+            <img class="article-image" src="${article.image_url}" alt="Image de l'article" />
+            <div class="article-content">${marked(article.texte)}</div>
+        `;
+    } else {
+        document.getElementById('article-container').innerHTML = `<p>Article non trouvé.</p>`;
+    }
+}
+
+function displayArticle() {
+    readArticlesJson()
+        .then(data => shoArticle(data))
+        .catch(error => {
+            alert(`Error : ${error.message}`);
+            window.location.href = homepath; 
+        });
+}
+
+displayArticle();
+
